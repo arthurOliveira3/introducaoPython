130c40adaa693bb06b12b3c237837ccbd55b7c27
Raj IFC <ruhtra.oliveira3@gmail.com>
2022-07-21 12:57:32 -0300

    Finished the math progra :)

--- matematica/Trabalho_Matemática.py
+++ matematica/Trabalho_Matemática.py
@@ -1,50 +1,54 @@
 import matplotlib.pyplot as plt
 
-funcao = 0
+def f(x):
+    return (
+        -3.510*x**5 + 0.571*x**4 + 16.126*x**3 + -7.193*x**2 + 4.705*x + 0.912
+    )
+
 começo = -20.0
 fim = 20.0
 x = 0
 intervaloPositivo = 100000000
 intervaloNegativo = -100000000
+x1 = 0
+x2 = 0
 
-while começo < fim:
+while começo <= fim:
     x = começo
-    funcao = -3.510*x**5 + 0.571*x**4 + 16.126*x**3 + -7.193*x**2 + 4.705*x + 0.912
-    if funcao > 0 and funcao < intervaloPositivo:
+    funcao = f(x)
+    if funcao < intervaloPositivo and funcao > 0:
         intervaloPositivo = funcao
+        x1 = x
         print(f'{x} | {intervaloPositivo}')
     elif funcao > intervaloNegativo and funcao < 0:
         intervaloNegativo = funcao
+        x2 = x
         print(f'{x} | {intervaloNegativo}')
     começo += 0.5
 
-
-media = intervaloPositivo + intervaloNegativo / 2
+media = (x1 + x2) / 2
 mediaAnterior = 0
+convergencia = 10 #10**-6
+iteracao = 0
 listaErros = []
 listaIteracoes = []
-convergencia = 1000
-iteracao = 0
 
-while abs(media - mediaAnterior) < convergencia and iteracao <=10:
-    if media > 0:
-        intervaloPositivo = media
-    elif media < 0:
-        intervaloNegativo = media
-    listaErros.append(media - mediaAnterior)
+while abs(media - mediaAnterior) < convergencia and iteracao < 100:
+    if f(media) > 0 and f(x1) > 0:
+        x1 = media
+    elif f(media) < 0 and f(x2) < 0:
+        x2 = media
+    listaErros.append(abs(media - mediaAnterior))
     listaIteracoes.append(iteracao)
     mediaAnterior = media
-    media = intervaloPositivo + intervaloNegativo / 2
+    media = (x1 + x2) / 2
     iteracao += 1
-    print("\n", media, mediaAnterior, iteracao)
+    print(f"\n{iteracao}: x1:{x1}  x2:{x2}  media:{media}  media Anterior:{mediaAnterior}")
 
-
-print(  f'\nMenor Intervalo Positivo encontrado:',intervaloPositivo,
-        f'\nMaior Intervalo Negativo encontrado:',intervaloNegativo,
-        "\n")
+print(f"\n\nMenor intervalo encontrado: {x1} à {x2}\n")
 
 plt.plot(listaIteracoes, listaErros)
-plt.title("Erros x Medias")
+plt.title("Erros x Iterações")
 plt.xlabel("Iterações")
-plt.ylabel("Erros Medias")
+plt.ylabel("Erros Médias")
 plt.show()
